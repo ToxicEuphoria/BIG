ed924b8d70e707943fcfa35fcba28d7fcab78566
John D. Storey <johndangerstorey@gmail.com>
2014-06-25 16:21:44 -0600

    Adds chart.js, bigger chart from drive

---
+++ assets/js/MainCtrl.js
@@ -0,0 +1,4 @@
+function MainCtrl($scope){
+	$scope.test = "Angular is Working";
+	$scope.graphFromGoogleSpreadsheet = '<img src="https://docs.google.com/spreadsheet/oimg?key=0AlSLfxaNuUNSdHBiX3NNcTJyMnFJTjlWOEtJcUxPS0E&oid=8&zx=7mtpx9sdakln" />';
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,54 @@
 				$('body').toggleClass('body-push-toleft');
 				$('#theMenu').toggleClass('menu-open');
 			});
+
+			var setupCanvas;
+			var data = {
+				labels : ["January","February","March","April","May","June","July"],
+				datasets : [
+					{
+						fillColor : "rgba(220,220,220,0.5)",
+						strokeColor : "rgba(220,220,220,1)",
+						pointColor : "rgba(220,220,220,1)",
+						pointStrokeColor : "#fff",
+						data : [65,59,-90,81,56,55,40,60]
+					},
+					{
+						fillColor : "rgba(151,187,205,0.5)",
+						strokeColor : "rgba(151,187,205,1)",
+						pointColor : "rgba(151,187,205,1)",
+						pointStrokeColor : "#fff",
+						data : [28,48,40,19,96,27,100]
+					}
+				]
+			}
+
+			// setupCanvas = function(canvas) {
+			//   var ctx, newWidth;
+			//   canvas = $(canvas);
+			//   newWidth = canvas.parent().width();
+			//   canvas.prop({
+			//     width: newWidth,
+			//     height: 200
+			//   });
+			//   ctx = canvas.get(0).getContext("2d");
+			//   return new Chart(ctx).Line(data);
+			// };
+
+			// (function(canvas) {
+			//   setupCanvas(canvas);
+			//   return $(window).resize(function() {
+			//     return setupCanvas(canvas);
+			//   });
+			// })("#mycanvas");
+
+			// //Get the context of the canvas element we want to select
+			// var ctx = $("#myChart").get(0).getContext("2d");
+			// //This will get the first returned node in the jQuery collection.
+			// var myChart = new Chart(ctx).Line(data);
+			// myChart.reDraw();
+
+			// location.reload();
+
+
 })(jQuery)
